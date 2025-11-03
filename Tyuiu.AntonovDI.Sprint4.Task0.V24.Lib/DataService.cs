757a96334b327d1d3e75b94984773e6f9133d65e
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-03 22:14:50 +0500

    Add project files.

---
+++ Tyuiu.AntonovDI.Sprint4.Task0.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AntonovDI.Sprint4.Task0.V24.Lib
+{
+    public class DataService : ISprint4Task0V24
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int count = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count *= array[i];
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint4.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AntonovDI.Sprint4.Task0.V24.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint4.Task0.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultEvenArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array =  new int[] { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            int res = ds.GetMultEvenArrEl(array);
+            int wait = 32;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
