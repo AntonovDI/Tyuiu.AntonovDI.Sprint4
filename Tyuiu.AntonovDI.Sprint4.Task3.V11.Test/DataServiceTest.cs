9d2f80fcadc5b325bed6e12a28b18415da988541
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-12 14:49:03 +0500

    Task3

---
+++ Tyuiu.AntonovDI.Sprint4.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.AntonovDI.Sprint4.Task3.V11.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint4.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5] { { 8, 6, 9, 4, 5 },
+                                           { 9, 4, 8, 5, 6 },
+                                           { 9, 7, 9, 8, 4 },
+                                           { 4, 6, 5, 7, 8 },
+                                           { 6, 6, 7, 6, 4 }};
+            int res = ds.Calculate(array);
+            int wait = 10;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint4.Task3.V11/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.AntonovDI.Sprint4.Task3.V11.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int[,] mtrx = new int[5, 5] { { 8, 6, 9, 4, 5 },
+                                       { 9, 4, 8, 5, 6 },
+                                       { 9, 7, 9, 8, 4 },
+                                       { 4, 6, 5, 7, 8 },
+                                       { 6, 6, 7, 6, 4 }};
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+        Console.Title = "Спринт #4 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* статическими значениями в диапазоне от 4 до 9. Подсчитайте количество   *");
+        Console.WriteLine("* нечетных элементов во всем массиве.                                     *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i,j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(mtrx );
+        Console.WriteLine("Количество нечётных элементов в массиве - " + res);
+    }
+}
