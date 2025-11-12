2d4545da60523dfbdee36937134c9c30f8730cf2
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-12 22:20:32 +0500

    Task6

---
+++ Tyuiu.AntonovDI.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AntonovDI.Sprint4.Task6.V4.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string[] mas = { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+            string[] res = ds.Calculate(mas);
+            string[] wait = { "Москва", "Самара" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
